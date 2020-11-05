bd9a014a1c1bc8b11a4013c87fcf434c23eed0d9
Matthew Kerr <matt777k@gmail.com>
2020-11-04 21:01:39 -0500

    got the navbar working on the homepage

---
+++ mk-react-portfolio/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import HomePageContainer from './components/HomePage'
+
+function App() {
+  return(
+<HomePageContainer/>
+)}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <HomePage/>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(HomePage, document.getElementById('root'))
+
+export default App;
---
+++ mk-react-portfolio/src/components/HomePage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import App from '../App';
+
+function HomePageContainer() {
+    return(
+        
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Matthew Kerr (Orlando, FL)</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="index.html"
+              >About <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="portfolio.html">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
+    )
+        
+}
+
+
+export default HomePageContainer;

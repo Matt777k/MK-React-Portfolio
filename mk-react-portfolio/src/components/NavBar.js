2b26d38cc886f1191a105e4783251a1fcdd3fa13
Matthew Kerr <matt777k@gmail.com>
2020-11-08 20:33:15 -0500

    fixed the refresh issue

--- mk-react-portfolio/src/components/NavBar.js
+++ mk-react-portfolio/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
@@ -19,19 +20,31 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="/">
-                About
-              </a>
+              <NavLink
+                to="/MK-React-Portfolio/aboutpage" className=
+                {window.location.pathname === "/MK-React-Portfolio/aboutpage"
+                  ? "nav-link active"
+                  : "nav-link"}
+                > About
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/MK-React-Portfolio/portfoliopage">
-                Portfolio
-              </a>
+            <NavLink
+                to="/MK-React-Portfolio/portfoliopage" className=
+                {window.location.pathname === "/MK-React-Portfolio/portfoliopage"
+                  ? "nav-link active"
+                  : "nav-link"}
+                > Portfolio
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/MK-React-Portfolio/contactpage">
-                Contact
-              </a>
+            <NavLink
+                to="/MK-React-Portfolio/contactpage" className=
+                {window.location.pathname === "/MK-React-Portfolio/contactpage"
+                  ? "nav-link active"
+                  : "nav-link"}
+                > Contact
+              </NavLink>
             </li>
           </ul>
         </div>
